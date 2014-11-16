538c2eda038ef9f762c68193de7b7c10a7f08afa
larsonj <joelarson4@gmail.com>
2014-11-16 08:00:34 -0800

    Bringing in mocha and chai as npm modules instead

--- test/maybeDemoTest.js
+++ test/maybeDemoTest.js
@@ -1,15 +1,15 @@
 if(window.mochaPhantomJS || document.location.href.indexOf('test') > -1) {
-    var TEST_PATH = '../test/';
+    var MODULES_PATH = '../node_modules/';
     //headjs 0.96 which ships with reveal.js doesn't support CSS loading...
     if(!window.mochaPhantomJS) {
         var cssElement = document.createElement('link');
         cssElement.setAttribute('rel', 'stylesheet');
-        cssElement.setAttribute('href', TEST_PATH + 'lib/mocha.css');
+        cssElement.setAttribute('href', MODULES_PATH + 'mocha/mocha.css');
         document.head.appendChild(cssElement);
     };
     head.js(
-        TEST_PATH + 'lib/mocha.js',
-        TEST_PATH + 'lib/chai.js',
+        MODULES_PATH + 'mocha/mocha.js',
+        MODULES_PATH + 'chai/chai.js',
         function() {
             var mochaElement = document.createElement('div');
             mochaElement.id = 'mocha';
@@ -20,7 +20,7 @@ if(window.mochaPhantomJS || document.location.href.indexOf('test') > -1) {
 
             window.assert = chai.assert;
 
-            head.js(TEST_PATH + 'demoTests.js', function() {
+            head.js('../test/demoTests.js', function() {
                 if (window.mochaPhantomJS) { mochaPhantomJS.run(); }
                 else { mocha.run(); }
             });
