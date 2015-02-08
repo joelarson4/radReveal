f82e6136cead1e42d5b51abc5d78a0e85b224b97
joelarson4 <joelarson4@gmail.com>
2015-02-07 18:57:35 -0800

    Changing how tests are loaded a bit

--- test/maybeTest.js
+++ test/maybeTest.js
@@ -1,7 +1,11 @@
+/**
+ * If running via phantom or in a browser with the a 'test' url parameter,
+ * this script will load mocha and chai and kick off test.
+ */
 if(window.mochaPhantomJS || document.location.href.indexOf('test') > -1) {
     var MODULES_PATH = 'node_modules/';
-    //headjs 0.96 which ships with reveal.js doesn't support CSS loading...
     if(!window.mochaPhantomJS) {
+        //headjs 0.96 which ships with reveal.js doesn't support CSS loading...
         var cssElement = document.createElement('link');
         cssElement.setAttribute('rel', 'stylesheet');
         cssElement.setAttribute('href', MODULES_PATH + 'mocha/mocha.css');
@@ -20,8 +24,18 @@ if(window.mochaPhantomJS || document.location.href.indexOf('test') > -1) {
 
             window.assert = chai.assert;
 
-            head.js('test/demoTests.js', function() {
-                if (window.mochaPhantomJS) { mochaPhantomJS.run(); }
+            var testScript = 'test/test.js';
+            var scripts = Array.prototype.slice.apply(document.getElementsByTagName('script'));
+            //expects to find only one
+            for(var si = 0; si < scripts.length; si++) {
+                if(scripts[si].src && scripts[si].src.indexOf('maybeTest.js?test=') >-1) {
+                    testScript = scripts[si].src.split('maybeTest.js?test=').pop(); //naive
+                    break;
+                }
+            };
+
+            head.js(testScript, function() {
+                if(window.mochaPhantomJS) { mochaPhantomJS.run(); }
                 else { mocha.run(); }
             });
         }
